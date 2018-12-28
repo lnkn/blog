2d96806c6c76087c5a4e6ce0551e257f57b47f61
lnkn <lincolndoyle@outlook.com>
2018-12-27 18:04:52 -0800

    remove comments on contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -37,16 +37,3 @@ const Contact = () => (
 
 export default Contact
 
-
-{/* <p>
-          <label>Your Name: <input type="text" name="name" /></label>   
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p> */}
