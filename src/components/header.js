928b32a9003ea848ba3d5267b92f3f6fbc01f46d
Zoe Kuhn <zoemkuhn@gmail.com>
2019-01-19 20:57:06 -0800

    Attempting to add a social media icon

--- src/components/header.js
+++ src/components/header.js
@@ -7,8 +7,11 @@ const Header = () => (
       <Link className="navlinks__about" to="about">About</Link>
       <Link className="navlinks__logo" to="/">Perfectly Salted</Link>
       <Link className="navlinks__contact" to="contact">Contact</Link>
+      <a href="[https://www.instagram.com/perfectlysalted/]">
+      <img title="Instagram" alt="RSS" src="https://socialmediawidgets.files.wordpress.com/2014/03/10_instagram.png" width="35" height="35" />
+      </a>
+      </div>
     </div>
-  </div>
 )
 
 export default Header
