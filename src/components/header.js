eb2786e41f374a29b68a7a48a4e8224669924a69
Zoe Kuhn <zoemkuhn@gmail.com>
2019-01-19 21:06:18 -0800

    Fixing header back to normal

--- src/components/header.js
+++ src/components/header.js
@@ -6,9 +6,6 @@ const Header = () => (
     <div className="navlinks">
       <Link className="navlinks__about" to="about">About</Link>
       <Link className="navlinks__logo" to="/">Perfectly Salted</Link>
-      <a href="(https://www.instagram.com/perfectlysalted/)">
-      <img title="Instagram" alt="RSS" src="https://socialmediawidgets.files.wordpress.com/2014/03/10_instagram.png" width="35" height="35" />
-      </a>
       <Link className="navlinks__contact" to="contact">Contact</Link>
       </div>
     </div>
