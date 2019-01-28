df02a0e7e459c498e2f1b25afc587db12e159897
Zoe Kuhn <zoemkuhn@gmail.com>
2019-01-27 22:29:50 -0800

    Getting rid of attempted instagram link

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 const Footer = ({ siteTitle }) => (
   <div className="Footer"  >
     <div>
-      <Link className="footer__logo" to="https://www.instagram.com/perfectlysalted/"> 
+      <Link className="footer__logo" to="/"> 
       </Link>
     </div>
   </div>
