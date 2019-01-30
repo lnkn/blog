bba4d23e35d150e89fc5aa963f9f3291176fa62f
lnkn <lincolndoyle@outlook.com>
2019-01-30 04:14:22 -0800

    test css grid gap

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 const Header = () => (
   <div className="header">
     <div className="navlinks nav__links">
-      <Link className="navlinks__about" to="about">About</Link>
+      <Link className="navlinks__about" to="about">About&nbsp;&nbsp;&nbsp;</Link>
       <Link className="navlinks__logo" to="/">Perfectly<br/>Salted</Link>
       <Link className="navlinks__contact" to="contact">Contact</Link>
       </div>
